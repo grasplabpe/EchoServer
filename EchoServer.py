d6540de742d40bac4585161a2a0c6970c893e5e2
GRASP LAP <44651047+grasplabpe@users.noreply.github.com>
2018-10-31 22:48:10 +0200

    Update EchoServer.py

--- EchoServer.py
+++ EchoServer.py
@@ -14,7 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print('Synapse Test Node Connected from:', addr)
+        print('Client Connected from:', addr)
         while True:
             data = conn.recv(1024)
             if not data:
